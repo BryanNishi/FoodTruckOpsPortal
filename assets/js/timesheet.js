53d55a4ad1a9f150faea3c7890e61b5802e77918
Bryan Nishi <Bryan.Nishi@gmail.com>
2019-03-11 21:04:04 -0600

    timesheet functionality and sales table

--- assets/js/timesheet.js
+++ assets/js/timesheet.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
     todaysDate();
 
     employeeList();
+
+    timesheet();
 });
 
 
@@ -23,99 +25,107 @@ var database = firebase.database();
 var selectedEmployee;
 var newEmployeeName;
 var employeeToRemove;
+var timestamp;
 var datestamp;
-var h;
-var m;
-var s;
 
 
 //real time clock
 function startTime() {
-    var today = new Date();
-    h = today.getHours();
-    m = today.getMinutes();
-    s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById("time").innerHTML =
-        h + ":" + m + ":" + s;
-    datestamp = h + ":" + m;
+    timestamp = moment().format("hh:mm:ss a");
+    document.getElementById("time").innerHTML = timestamp;
+
     var t = setTimeout(startTime, 500);
 }
-function checkTime(i) {
-    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
-    return i;
-}
+
 
 function todaysDate() {
-    var date = new Date();
-    document.getElementById("date").innerHTML = date.toLocaleDateString();
+    date = moment().format("dddd MMM Do");;
+    document.getElementById("date").innerHTML = date;
+
 }
 
+//load employee list
 function employeeList() {
-    //clear current schedule and employee list for refresh
+    //clear current schedule for refresh
     $("#table").empty();
-    $("#employeeList").html("<option selected='selected'>Choose Employee </option>");
 
-    database.ref().on("value", function () {
-        console.log("data" + snapshot.val());
-
+    //load existing values
+    var employeeList = firebase.database().ref('employees/');
+    employeeList.on('value', function (snapshot) {
+        var list = snapshot.val();
+        $("#employeeList").empty();
+        $("#employeeList").html("<option selected='selected'>Choose Employee </option>");
+        for (var name in list) {
+            $("#employeeList").append("<option value=" + name + ">" + name + "</option>");
+            $("#employees").append("<option value=" + name + ">" + name + "</option>");
+        }
     });
-    //on database update
-    database.ref().on("child_added", function (childSnapshot) {
-        //console.log("database ", childSnapshot);
+}
+//load time sheet table
+function timesheet() {
+    var timeTable = firebase.database().ref('employees/');
+    timeTable.on('value', function (snapshot) {
+        var table = snapshot.val();
+        console.log("snapshot value", table);
+        for (var timeclock in table) {
+            console.log(timeclock);
+        }
 
-        $("#employeeList").append("<option value=" + childSnapshot.val().EmployeeName + ">" + childSnapshot.val().EmployeeName + "</option>");
-        $("#employees").append("<option value=" + childSnapshot.val().EmployeeName + ">" + childSnapshot.val().EmployeeName + "</option>");
-        $("#table").append("<tr><td>" + childSnapshot.val().EmployeeName + "</td><td>" + childSnapshot.val().punchIN + "</td><td>" + childSnapshot.val().punchOUT + "</td></tr>")
+        for (var name in table) {
+
+
+            $("#table").append("<tr><td>" + name + "</td><td>" + name.timeclock + "</td><td> </td></tr>")
+        }
+        for (var punchOUT in table) {
+            $("#table").append("<tr><td></td><td></td><td>" + punchOUT + "</td></tr>")
+        }
     });
 }
 
 /************************************************************Timesheet functions **********************************************/
 
-
-
-//Clock In Function
-function punchIn() {
-    if (selectedEmployee == null) {
-        alert("Please Select An Employee")
-    } else {
-        datestampIn = datestamp;
-        alert(selectedEmployee + " Clocked in at " + datestampIn)
-        $("#table").append("<tr><td>" + selectedEmployee + "</td><td>" + datestampIn + "</td><td> </td></tr>")
-
-        // Change what is saved in firebase
-        database.ref().push({
-            selectedEmployee: selectedEmployee,
-            punchIN: datestampIn,
-            date: date,
-        });
-    }
-}
-
-//Clock Out Function
-function punchOut() {
-    if (selectedEmployee == null) {
-        alert("Please Select An Employee")
-    } else {
-        datestampOut = datestamp;
-        alert(selectedEmployee + " Clocked out at " + datestampOut)
-        $("#table").append("<tr><td>" + selectedEmployee + "</td><td>  </td><td>" + datestampOut + "</td></tr>")
-
-        // Change what is saved in firebase
-        database.ref().push({
-            selectedEmployee: selectedEmployee,
-            punchOUT: datestampOUT,
-            date: date,
-        });
-    }
-}
-
 // Employee Dropdown list selection change handler
 function employeeHandler() {
     selectedEmployee = $('#employeeList').val().trim();
-
 }
+
+//Clock In Function
+function punchIn() {
+    if (selectedEmployee === undefined) {
+        alert("Please Select An Employee")
+    } else {
+        timestampIn = timestamp;
+        alert(selectedEmployee + " Clocked in at " + timestampIn)
+        $("#table").append("<tr><td>" + selectedEmployee + "</td><td>" + timestampIn + "</td><td> </td></tr>")
+
+        // Change what is saved in firebase
+        database.ref("employees/" + selectedEmployee + "/timeclock/IN/").set({
+            punchIN: timestampIn,
+            date: datestamp,
+        });
+
+    }
+}
+
+
+//Clock Out Function
+function punchOut() {
+    if (selectedEmployee === undefined) {
+        alert("Please Select An Employee")
+    } else {
+        timestampOut = timestamp;
+        alert(selectedEmployee + " Clocked in at " + timestampOut)
+        $("#table").append("<tr><td>" + selectedEmployee + "</td><td></td><td> " + timestampOut + "</td></tr>")
+
+        // Change what is saved in firebase
+        database.ref("employees/" + selectedEmployee).push({
+            punchOUT: timestampOut,
+            date: datestamp,
+        });
+    }
+}
+
+
 
 //Add employee modal function
 function addEmployee() {
@@ -127,17 +137,21 @@ function addEmployee() {
         // Get inputs
         newEmployeeName = $("#nameInput").val().trim();
 
+        $("#employeeList").append("<option value=" + newEmployeeName + ">" + newEmployeeName + "</option>");
+        $("#employees").append("<option value=" + newEmployeeName + ">" + newEmployeeName + "</option>");
+
         // Change what is saved in firebase
-        database.ref().child().push({
-            EmployeeName: newEmployeeName,
+        database.ref("employees/" + newEmployeeName).set({
+            name: newEmployeeName,
         });
         alert(newEmployeeName + " added!");
+        // $("#add-modal-content").html("Success!");
+        employeeList();
     }
 };
 
 function employeeRemoveHandler() {
     employeeToRemove = $("#employees").val().trim();
-    console.log("remove Select", employeeToRemove);
 }
 
 //remove employee modal function
@@ -145,9 +159,8 @@ function removeEmployee() {
     console.log("employee to remove", employeeToRemove);
 
     // Change what is saved in firebase
-    database.ref().remove({
-        EmployeeName: employeeToRemove,
-    });
+    database.ref("employees/" + employeeToRemove).remove();
     alert(employeeToRemove + " Removed!");
-
+    // $("#remove-modal-content").html(employeeToRemove + " Removed!");
+    employeeList();
 };
